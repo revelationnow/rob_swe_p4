3a4bccb344c88b016eff14f303ee46ebfdba4c59
Anoop Ramakrishna <anoop.ramakrishna@live.com>
2020-02-01 06:35:28 -0800

    Starting

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/anoop/Work/udacity/robotics_sw_engineer/p2/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/melodic".split(';') if "/opt/ros/melodic" != "" else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/anoop/Work/udacity/robotics_sw_engineer/p2/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/melodic/lib:/home/anoop/Downloads/CUDA/cuda/lib64:/home/anoop/Downloads/CUDA/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/cuda/extras/CUPTI/lib64"
+export PWD="/home/anoop/Work/udacity/robotics_sw_engineer/p2/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/anoop/Work/udacity/robotics_sw_engineer/p2/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/anoop/Work/udacity/robotics_sw_engineer/p2/src:$ROS_PACKAGE_PATH"
